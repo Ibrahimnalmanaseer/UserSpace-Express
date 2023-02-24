57f58f8eac62b8d98b594365142f769dbfe7c6f6
Ibrahim Almanaseer <ibrahimnalmanaseer@gmail.com>
2023-02-24 15:00:15 +0000

    r

--- routes/addDataRouteHandler.js
+++ routes/addDataRouteHandler.js
@@ -5,14 +5,14 @@ const { UserInfo } = require('../app');
 async function addDataRouteHandler(request, response) {
   const { Email, Name, Sector, AgreeToTerms } = request.body;
 
-  // Search for a user data record with the same email as the request
+  
   const existingUserData = await UserInfo.findOne({ Email });
 
   if (existingUserData) {
-    // If a record with the same email exists, return that record in the response
+    
     response.send(existingUserData);
   } else {
-    // If a record with the same email does not exist, create a new record with the provided data
+    
     const newUserData = await UserInfo.create({
       Email,
       Name,
@@ -20,7 +20,7 @@ async function addDataRouteHandler(request, response) {
       AgreeToTerms
     });
 
-    // Return the newly created record in the response
+    
     response.send(newUserData);
   }
 }
